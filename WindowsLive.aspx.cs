9f6926aa5e64448d10ffe6bf68a9b8a6538065a9
Ashwin Gaddam <gaddam_ashwin@yahoo.com>
2013-07-16 15:36:39 -0400

    Minor Bug fixes.

--- WindowsLive.aspx.cs
+++ WindowsLive.aspx.cs
@@ -21,33 +21,33 @@
 
 		protected void Page_Load(object sender, EventArgs e)
 		{
-			if (string.Equals("localhost", this.Request.Headers["Host"].Split(':')[0], StringComparison.OrdinalIgnoreCase))
+            //if (string.Equals("localhost", this.Request.Headers["Host"].Split(':')[0], StringComparison.OrdinalIgnoreCase))
+            //{
+            //    this.localhostDoesNotWorkPanel.Visible = true;
+            //    var builder = new UriBuilder(this.publicLink.NavigateUrl);
+            //    builder.Port = this.Request.Url.Port;
+            //    this.publicLink.NavigateUrl = builder.Uri.AbsoluteUri;
+            //    this.publicLink.Text = builder.Uri.AbsoluteUri;
+            //}
+            //else
+            //{
+			IAuthorizationState authorization = windowsLiveClient.ProcessUserAuthorization();
+			if (authorization == null)
 			{
-				this.localhostDoesNotWorkPanel.Visible = true;
-				var builder = new UriBuilder(this.publicLink.NavigateUrl);
-				builder.Port = this.Request.Url.Port;
-				this.publicLink.NavigateUrl = builder.Uri.AbsoluteUri;
-				this.publicLink.Text = builder.Uri.AbsoluteUri;
+				// Kick off authorization request
+                //windowsLiveClient.RequestUserAuthorization();
+				windowsLiveClient.RequestUserAuthorization(scope: new[] { WindowsLiveClient.Scopes.Basic }); // this scope isn't even required just to log in
+
+				// alternatively you can ask for more information
+				// windowsLiveClient.RequestUserAuthorization(scope: new[] { WindowsLiveClient.Scopes.SignIn, WindowsLiveClient.Scopes.Emails, WindowsLiveClient.Scopes.Birthday });
 			}
 			else
 			{
-				IAuthorizationState authorization = windowsLiveClient.ProcessUserAuthorization();
-				if (authorization == null)
-				{
-					// Kick off authorization request
-                    //windowsLiveClient.RequestUserAuthorization();
-					windowsLiveClient.RequestUserAuthorization(scope: new[] { WindowsLiveClient.Scopes.Basic }); // this scope isn't even required just to log in
+				IOAuth2Graph oauth2Graph = windowsLiveClient.GetGraph(authorization);
 
-					// alternatively you can ask for more information
-					// windowsLiveClient.RequestUserAuthorization(scope: new[] { WindowsLiveClient.Scopes.SignIn, WindowsLiveClient.Scopes.Emails, WindowsLiveClient.Scopes.Birthday });
-				}
-				else
-				{
-					IOAuth2Graph oauth2Graph = windowsLiveClient.GetGraph(authorization);
-
-					this.nameLabel.Text = HttpUtility.HtmlEncode(oauth2Graph.Name);
-				}
+				this.nameLabel.Text = HttpUtility.HtmlEncode(oauth2Graph.Name);
 			}
+            //}
 		}
 	}
 }
